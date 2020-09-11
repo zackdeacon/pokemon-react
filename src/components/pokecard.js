c621a315cee1e3bfbd32b6e3ad70e854a88fbd74
Zachary Deacon <zackdeacon347@gmail.com>
2020-09-11 14:29:31 -0700

    fixed styling colors and responsiveness

--- src/components/pokecard.js
+++ src/components/pokecard.js
@@ -18,12 +18,11 @@ export default function Pokecard(props) {
   <div className="card CARD animate__animated animate__fadeIn" >
   <div className="card-body">
     <h3 className="card-title pkmnName">{props.name}</h3>
-    <div className="pkmnImg img-fluid">
-    <img className="hvr-float-shadow" src={imgSrc} alt="pkmn img"/>
+    <div className="pkmnImg">
+    <img className="hvr-float-shadow img-fluid" src={imgSrc} alt="pkmn img"/>
     </div>
     <div className="bottomBox">
     <h5 className="pkmnType">Type:{props.type}</h5>
-    <br></br>
     <h5 className="pkmnExp">EXP:{props.base_experience}</h5>
     </div>
   </div>
